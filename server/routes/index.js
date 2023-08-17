b39322e5a91ec6fc7428b7572c3570eaf9ff1f10
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-08-17 19:06:29 -0400

    added

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,6 @@ const api = require('./api')
 const path = require('path')
 router.use('/api',api)
 router.use((req,res)=>{
-    res.sendFile(path.join(__dirname, '../../client/build.index.html'))
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
 })
 module.exports = router
